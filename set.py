3ab363276b0f43bb50120bfc5d73be8126840424
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-12-25 23:34:36 +0530

    changes

---
+++ set.py
@@ -0,0 +1,36 @@
+# set = {1,2,3,4}
+# print(set)
+
+# myset = set()
+# myset.add(1)
+# myset.add(2)
+# myset.add(3)
+# myset.remove(1)
+
+# print(myset)
+# myset.clear()
+# print(myset)
+
+# myset = set()
+# myset.add(1)
+# myset.add(2)
+# myset.add(3)
+# print(myset)
+
+# myset.pop()
+# print(myset)
+
+# myset1 = {"hello","string","world"}
+myset= set()
+myset.add("hello")
+myset.add("happy")
+myset.add("world")
+myset.pop()
+print(myset)
+
+
+set = {1,2,3,3,4,5,4};
+
+for i in set:
+    print(i)
+
