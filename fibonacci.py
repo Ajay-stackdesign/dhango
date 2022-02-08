5dbbf351d4043c3fcd716628f61103afa76fef33
Ajay-stackdesign <ajaygsahani968@gmail.com>
2022-02-08 23:40:08 +0530

    chages

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+# a = int(input("enter the number"))
+# b = int(input("enter the number"))
+# n = int(input("enter the nnumber"))
+a = 6
+b = 7
+n = 10
+
+while n-2:
+    c= a+ b
+    a =b
+    b =c
+    print(c, end=" ")
+    n = n -1
