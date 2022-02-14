2c25a82275d95cdccbe88fc9c63db7bc8025594d
Ajay-stackdesign <ajaygsahani968@gmail.com>
2022-02-14 23:41:05 +0530

    changes

--- factorialNumber.py
+++ factorialNumber.py
@@ -1,16 +1,35 @@
-def fact(num):
-    if num == 1:
-        return num
-    else:
-        result = num * fact (num -1)
-        return result
-        # print(num * fact(num -1))
+# def fact(num):
+#     if num == 1:
+#         return num
+#     else:
+#         result = num * fact (num -1)
+#         return result
+#         # print(num * fact(num -1))
+#
+# number = 5
+#
+# factor = fact(number)
+# print(factor)
+#
+#
+#
+# arraay = [2,2,3,4]
+#
+# print(arraay[::-1])
+#
+#
+# print alternate elements of an arrat:
 
-number = 5
+def alternate(arr):
+    output = []
 
-factor = fact(number)
-print(factor)
+    for i in range(1,len(arr)+1):
+        if  arr[i] % 2 == 0:
+            output.append(i)
+        return output
 
-
+a = [1,2,3,4,5]
+result = alternate(a)
+print(result)
 
 
