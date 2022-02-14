7f535778a8ae355da8c6252a6f93e7746758047f
Ajay-stackdesign <ajaygsahani968@gmail.com>
2022-02-14 23:45:43 +0530

    changes

--- binarysearch.py
+++ binarysearch.py
@@ -57,60 +57,60 @@
 #     li = len(arr) - 1
 #
 #     while lo <= li:
-#         mid = len(arr) // 2
+# #         mid = len(arr) // 2
+# #
+# #         if arr[mid] == query:
+# #             return mid
+# #
+# #         elif arr[mid] < query:
+# #             return mid+1
+# #         elif arr[mid] > query:
+# #             return mid-1
+# #         return -1
+# #
+# # arr = [2,3,4,5,6,7,9]
+# # query = 4
+# #
+# # result = binary_Search(arr,query)
+# # print(result)
+# def merge_sort(arr):
+#     if len(arr) <= 1:
+#         return
 #
-#         if arr[mid] == query:
-#             return mid
+#     mid = len(arr) // 2
+#     left = arr[:mid]
+#     right = arr [mid:]
 #
-#         elif arr[mid] < query:
-#             return mid+1
-#         elif arr[mid] > query:
-#             return mid-1
-#         return -1
+#     merge_sort(left)
+#     merge_sort(right)
 #
-# arr = [2,3,4,5,6,7,9]
-# query = 4
+#     merge_sort_list(left, right, arr)
+#     # binar_search(arr, )
 #
-# result = binary_Search(arr,query)
-# print(result)
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return
-
-    mid = len(arr) // 2
-    left = arr[:mid]
-    right = arr [mid:]
-
-    merge_sort(left)
-    merge_sort(right)
-
-    merge_sort_list(left, right, arr)
-    # binar_search(arr, )
-
-def merge_sort_list(a, b, arr):
-    lena = len(a)
-    lenb = len(b)
-
-    i = j = k = 0
-
-    while i < lena and j < lenb:
-        if a[i] <= b[j]:
-            arr[k] = a[i]
-            i+=1
-        else:
-            arr[k] = b[j]
-            j+=1
-        k+=1
-
-    while i < lena:
-        arr[k] = a[i]
-        i+=1
-        k+=1
-
-    while j < lenb:
-        arr[k] = b[j]
-        j+=1
-        k+=1
+# def merge_sort_list(a, b, arr):
+#     lena = len(a)
+#     lenb = len(b)
+#
+#     i = j = k = 0
+#
+#     while i < lena and j < lenb:
+#         if a[i] <= b[j]:
+#             arr[k] = a[i]
+#             i+=1
+#         else:
+#             arr[k] = b[j]
+#             j+=1
+#         k+=1
+#
+#     while i < lena:
+#         arr[k] = a[i]
+#         i+=1
+#         k+=1
+#
+#     while j < lenb:
+#         arr[k] = b[j]
+#         j+=1
+#         k+=1
 
 def binar_search(arr , query):
     lo = 0
