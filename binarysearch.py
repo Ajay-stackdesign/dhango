912f5e1a85b76121c7ce470f8eed740951d88cec
Ajay-stackdesign <ajaygsahani968@gmail.com>
2022-02-21 23:42:52 +0530

    changes

--- binarysearch.py
+++ binarysearch.py
@@ -1,19 +1,19 @@
-# def binarySearch(nums, target):
-#     lo = 0
-#     li = len(nums) - 1
-#     while lo <= li:
-#         mid = lo + li // 2
-#         if nums[mid] == target:
-#             return mid
-#         elif nums[mid] < target:
-#             return mid + 1
-#         elif nums[mid] > target:
-#             return mid - 1
-#     return -1
-# nums = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
-# target = 38
-# result = binarySearch(nums, target)
-# print(result)
+def binarySearch(nums, target):
+    lo = 0
+    li = len(nums) - 1
+    while lo <= li:
+        mid = lo + li // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            return mid + 1
+        elif nums[mid] > target:
+            return mid - 1
+    return -1
+nums = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+target = 38
+result = binarySearch(nums, target)
+print(result)
 # def binarySearch(arr, l, r, x):
 #     # Check base case
 #     if r >= l:
