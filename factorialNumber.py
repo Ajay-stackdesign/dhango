d45a021d17ddaaa31bc710bb3ac35f0f9d19701a
Ajay-stackdesign <ajaygsahani968@gmail.com>
2022-03-08 23:56:36 +0530

    changes

--- factorialNumber.py
+++ factorialNumber.py
@@ -1,15 +1,15 @@
-# def fact(num):
-#     if num == 1:
-#         return num
-#     else:
-#         result = num * fact (num -1)
-#         return result
-#         # print(num * fact(num -1))
-#
-# number = 5
-#
-# factor = fact(number)
-# print(factor)
+def fact(num):
+    if num == 1:
+        return num
+    else:
+        result = num * fact (num -1)
+        return result
+        # print(num * fact(num -1))
+
+number = 5
+
+factor = fact(number)
+print(factor)
 #
 #
 #
