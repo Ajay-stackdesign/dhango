d5dfe55edb50a1491c35faf0981a70c2533b5ce3
Ajay-stackdesign <ajaygsahani968@gmail.com>
2022-03-19 23:17:39 +0530

    changes

---
+++ py.py
@@ -0,0 +1,26 @@
+from math import ceil
+
+
+print(round(1.7))
+
+# str = "hello"
+
+# str2 = list(str)
+# print(str2)
+
+list1 = ["ajy", "ajay"]
+
+str3 = "".join(list1)
+print(str3)
+
+str2 = list(str3)
+print(str2)
+
+print(str3)
+
+tuple1 = ("ajy", "ajay")
+
+str4 = "".join(tuple1)
+print(str4)
+# list2 = list(tuple1)
+# print(list2)
